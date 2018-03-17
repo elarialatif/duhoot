982508bed4b6cbf64b11fd5c5f8e47716893d9d1
Elaria <elarya.latif@gmail.com>
2018-03-17 14:39:05 +0200

    odd row

---
+++ assets/js/ui.js
@@ -0,0 +1,8 @@
+// $(document).ready(function() {
+//     console.log("ready!");
+//     $("#card.card1 img").hover(function() {
+//         $('.flyout').removeClass('hidden');
+//     }, function() {
+//         $('.flyout').addClass('hidden');
+//     });
+// });
